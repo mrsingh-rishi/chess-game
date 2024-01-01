f9c668ea4c33a99cfaaad57812f0ee00a9c6f65d
Rishi Singh <rs3949427@gmail.com>
2024-01-01 13:24:30 +0530

    Completed Chess Board(starting UI)

--- src/components/ChessBoard/ChessBoard.tsx
+++ src/components/ChessBoard/ChessBoard.tsx
@@ -20,21 +20,23 @@ for (let i = 0; i < 8; i++) {
   pieces.push({ image: "assets/images/pawn_w.png", x: i, y: 1 });
 }
 
-// white rooks
-pieces.push({ image: "assets/images/rook_w.png", x: 0, y: 0 });
-pieces.push({ image: "assets/images/rook_w.png", x: 7, y: 0 });
+// rest of the pieces
 
-// black rooks
-pieces.push({ image: "assets/images/rook_b.png", x: 0, y: 7 });
-pieces.push({ image: "assets/images/rook_b.png", x: 7, y: 7 });
+for (let p = 0; p < 2; p++) {
+  const type = p == 0 ? "b" : "w";
+  const y = p == 0 ? 7 : 0;
+  pieces.push({ image: `assets/images/rook_${type}.png`, x: 0, y: y });
+  pieces.push({ image: `assets/images/rook_${type}.png`, x: 7, y: y });
 
-// white knight
-pieces.push({ image: "assets/images/knight_w.png", x: 1, y: 0 });
-pieces.push({ image: "assets/images/knight_w.png", x: 6, y: 0 });
+  pieces.push({ image: `assets/images/knight_${type}.png`, x: 1, y: y });
+  pieces.push({ image: `assets/images/knight_${type}.png`, x: 6, y: y });
 
-// black knight
-pieces.push({ image: "assets/images/knight_b.png", x: 1, y: 7 });
-pieces.push({ image: "assets/images/knight_b.png", x: 6, y: 7 });
+  pieces.push({ image: `assets/images/bishop_${type}.png`, x: 2, y: y });
+  pieces.push({ image: `assets/images/bishop_${type}.png`, x: 5, y: y });
+
+  pieces.push({ image: `assets/images/king_${type}.png`, x: 3, y: y });
+  pieces.push({ image: `assets/images/queen_${type}.png`, x: 4, y: y });
+}
 
 const ChessBoard = () => {
   let board = [];
